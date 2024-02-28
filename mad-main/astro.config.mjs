c2717a782dc7d82c7632ac161a34fb9244c824a1
EseToni <falsigfx@gmail.com>
2024-02-28 21:34:07 +0100

    [ADD] netlify

--- mad-main/astro.config.mjs
+++ mad-main/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
